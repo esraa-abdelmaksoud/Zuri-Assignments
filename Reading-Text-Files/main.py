ea0bcda86e049b9fb629d1f08ee35ed116499629
esraa-abdelmaksoud <esraa.e.abdelmaksoud@gmail.com>
2022-05-24 21:46:47 +0200

    update 24-5

--- Reading-Text-Files/main.py
+++ Reading-Text-Files/main.py
@@ -3,6 +3,9 @@
 # count_words("The cake is done. It is a big cake!") 
 # --> {"cake":2, "big":1, "is":2, "the":1, "a":1, "it":1}
 
+from collections import Counter
+import re
+
 def read_file_content(filename):
     # [assignment] Add your code here 
     with open (filename) as f:
@@ -14,13 +17,16 @@ def read_file_content(filename):
 def count_words():
     text = read_file_content("./story.txt")
     # [assignment] Add your code here
-    full_text = "".join(text)
-    words = full_text.split()
+
+    # Join text lines and convert to lowercase
+    full_text = "".join(text).lower()
+
+    # Cleaning text to remove symbols, question marks, etc
+    clean_text = re.sub('[^a-z ]', '', full_text)
+    words = clean_text.split()
 
     # Consider any of the two solutions
-
     # Solution 1
-    # from collections import Counter
     # word_dict = Counter(words)
 
     # Solution 2
@@ -32,5 +38,3 @@ def count_words():
             word_dict[w] = 1
 
     return word_dict
-
-print(count_words())
